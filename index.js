55111250da89a9676d22c645c2e06f21afed0bed
wesleybertipaglia <wesleybertipaglia@gmail.com>
2023-06-24 16:20:34 -0300

    feat: add info in books

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = process.env.PORT || 3001;
 
 server.use(express.json());
 
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // database
 const books = require('./src/data/books.json');
 
