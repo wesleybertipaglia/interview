5089f590ade79c0d55e6013db01b096550872881
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-05-30 12:54:15 -0300

    feat: bubble sort implementation in c

--- algorithms/sorting/bubble-sort.c
+++ algorithms/sorting/bubble-sort.c
@@ -17,6 +17,26 @@ int main(void)
 
 int *sort(int arr[], int len)
 {
-    // This is a placeholder implementation
+    /*  pseudocode
+        // iterate over the array
+            iterate over the array
+            - the last element (because it is already in the right place)
+            - 1 (because the arr starts in 0)
+                if the current element is bigger than the next swap the elements
+        // return the arr
+    */
+
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - i - 1; j++) // i=0 -> arr[n]; i=1 -> arr[n-1]
+        {
+            if (arr[j] > arr[j + 1]) // current > next
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1]; // swap elements
+                arr[j + 1] = temp;
+            }
+        }
+    }
     return arr;
 }
